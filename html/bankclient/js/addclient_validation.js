380f921db611e25ade91b9af9feb8378c3228049
Denis Makarov <denm2000@gmail.com>
2015-02-11 18:11:29 +0200

    HTML frontend added

---
+++ html/bankclient/js/addclient_validation.js
@@ -0,0 +1,50 @@
+﻿function checkForm() {
+        var name=$("#name").val();		
+		var email = $("#email").val();
+		var city = $("#city").val();
+		var balance = parseFloat($("#balance").val());
+		
+        if (name.length == 0) {
+            $("#nameError").html("Пожалуйста, укажите имя");
+            return false;
+        }
+		
+		var nameArr = name.split(" ");
+		if (nameArr.length != 2) {
+			$("#nameError").html("Пожалуйста, укажите имя");
+			return false;
+		}
+		
+		for(var i=0;i<nameArr.length;i++) {
+        var n = nameArr[i];
+            if (n.length == 0) {
+                $("#nameError").html("Пожалуйста, укажите Имя и Фамилию");
+                return false;
+            }
+        }  
+		
+		if (city.length == 0) {
+            $("#cityError").html("Пожалуйста, укажите город");
+            return false;
+        }
+		
+        if (!email.match(".+@.+\.[a-zA-Z]{2,3}")) {
+            $("#emailError").html("неверный формат e-mail");
+            return false;
+        }
+		
+		if ( balance != null && balance < 0.0) {
+            $("#balanceError").html("Сумма должна быть положительной");
+            return false;
+        }
+		if (isNaN(balance)) {
+            $("#balanceError").html("Пожалуйста, введите сумму");
+            return false;
+        }
+			
+		
+		$("#nameError").html("");
+		$("#emailError").html("");
+		$("#cityError").html("");
+        return true;
+}
---
+++ html/bankomat/js/sum_validation.js
@@ -0,0 +1,14 @@
+﻿function isNumeric() {
+        var sum=parseFloat($("#sum").val());
+		
+        if ( sum != null && sum < 0.0) {
+            $("#sumError").html("Сумма должна быть положительной");
+            return false;
+        }
+		if (isNaN(sum)) {
+            $("#sumError").html("Пожалуйста, введите сумму");
+            return false;
+        }
+		$("#sumError").html("");
+        return true;
+}
